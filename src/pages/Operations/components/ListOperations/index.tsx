5b2b39290809f2a0fce8973f459c1af87100c038
Igor Piedade <igorpiedade@Igors-MacBook-Air.local>
2022-08-04 12:10:05 -0400

    ...

--- src/pages/Operations/components/ListOperations/index.tsx
+++ src/pages/Operations/components/ListOperations/index.tsx
@@ -1,38 +1,62 @@
 import { CalendarBlank, CaretDown } from "phosphor-react";
 import { DataFilter, Header, OperationsTable } from "./styled";
 
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../../../../services/api";
+
+interface IOperationData {
+    id: string;
+    date: Date;
+    assetName: string;
+}
+
 export function ListOperations() {
+
+    const { data, isFetching } = useQuery<IOperationData>(['operation'], async () => {
+        const response = await api.get('operation');
+
+        console.log(response.data);
+
+        return response.data;
+    })
+
     return (
         <>
             <Header>
                 <h1>OPERATIONS</h1>
                 <DataFilter>
                     <CalendarBlank />
-                    <span>7 days</span>
+                    <span>ALL</span>
                     <CaretDown />
                 </DataFilter>
             </Header>
-
+            {/* {isFetching && <span>Loading...</span>}
             <OperationsTable>
-                <tr>
-                    <th>DATE</th>
-                    <th>ASSET</th>
-                    <th>ORDER</th>
-                    <th>QTY</th>
-                    <th>PRICE</th>
-                    <th>WALLET</th>
-                    <th>AMOUT</th>
-                </tr>
-                <tr>
-                    <td>16-jun-22</td>
-                    <td>NYS:O</td>
-                    <td>BUY</td>
-                    <td>10</td>
-                    <td>$57,90</td>
-                    <td>WEBULL</td>
-                    <td>$579,00</td>
-                </tr>
-            </OperationsTable>
+                <tbody>
+                    <tr>
+                        <th>DATE</th>
+                        <th>ASSET</th>
+                        <th>ORDER</th>
+                        <th>QTY</th>
+                        <th>PRICE</th>
+                        <th>WALLET</th>
+                        <th>AMOUT</th>
+                    </tr>
+                    {data?.map(operation => {
+                        return (
+                            <tr key={operation.id}>
+                                <td>{operation.date}</td>
+                                <td>NYS:O</td>
+                                <td>BUY</td>
+                                <td>10</td>
+                                <td>$57,90</td>
+                                <td>WEBULL</td>
+                                <td>$579,00</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </OperationsTable> */}
         </>
     );
 }
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("token")}`
+        authorization: `Bearer ${sessionStorage.getItem("token")}`
     }
 });
